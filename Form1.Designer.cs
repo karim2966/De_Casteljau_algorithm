3f12b3afe1a16fe678e5138df295054390350f1a
karim2966 <krymqmash123@gmail.com>
2026-02-03 13:17:34 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ite_homework_karim_296610
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDrawPoint = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.BtnDeCas = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnDrawPoint
+            // 
+            this.BtnDrawPoint.Location = new System.Drawing.Point(12, 32);
+            this.BtnDrawPoint.Name = "BtnDrawPoint";
+            this.BtnDrawPoint.Size = new System.Drawing.Size(110, 68);
+            this.BtnDrawPoint.TabIndex = 0;
+            this.BtnDrawPoint.Text = "Draw Points";
+            this.BtnDrawPoint.UseVisualStyleBackColor = true;
+            this.BtnDrawPoint.Click += new System.EventHandler(this.BtnDrawPoint_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 106);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 68);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // BtnDeCas
+            // 
+            this.BtnDeCas.Location = new System.Drawing.Point(12, 180);
+            this.BtnDeCas.Name = "BtnDeCas";
+            this.BtnDeCas.Size = new System.Drawing.Size(110, 68);
+            this.BtnDeCas.TabIndex = 2;
+            this.BtnDeCas.Text = "De Casteljau";
+            this.BtnDeCas.UseVisualStyleBackColor = true;
+            this.BtnDeCas.Click += new System.EventHandler(this.BtnDeCas_Click);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel.Location = new System.Drawing.Point(161, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(824, 555);
+            this.panel.TabIndex = 3;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+           
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1027, 593);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.BtnDeCas);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.BtnDrawPoint);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digitizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDrawPoint;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button BtnDeCas;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ite_homework_karim_296610
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        // Coordinates and point counts
+        int[] PX = new int[25];
+        int[] PY = new int[25];
+        int PointIndex = 0;
+        // Draw button status
+        bool status = false;
+        // De Casteljau algorithm button status
+        bool BtnDeCas1 = false;
+        public Form()
+        {
+            InitializeComponent();
+            //
+            panel.MouseClick += panel_MouseClick;
+        }
+
+        public float DeCasteljau(float t, float[] coefficients)
+        {
+            float[] beta = coefficients;
+            int n = beta.Length;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < (n - i); j++)
+                {
+                    beta[j] = beta[j] * (1 - t) + beta[j + 1] * t;
+                }
+            }
+            return beta[0];
+        }
+        // Determine the maximum number in an array
+        public int MaxPoint(int[] PXY)
+        {
+            int[] P = PXY;
+            int MaxNum = P[0];
+            for (int i = 0; i < PointIndex - 1; i++)
+            {
+                if (P[i + 1] > MaxNum)
+                {
+                    MaxNum = P[i + 1];
+                }
+            }
+            return MaxNum;
+        }
+        // Determine the minimum number in an array
+        public int MinPoint(int[] PXY)
+        {
+            int[] P = PXY;
+            int MinNum = P[0];
+            for (int i = 0; i < PointIndex - 1; i++)
+            {
+                if (P[i + 1] <= MinNum)
+                {
+                    MinNum = P[i + 1];
+                }
+            }
+            return MinNum;
+        }
+        // Change the draw button status
+        private void BtnDrawPoint_Click(object sender, EventArgs e)
+        {
+            if (status == false)
+            {
+                status = true;
+                BtnDeCas1 = false;
+            }
+        }
+        // Drawing
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            // Line
+            Graphics g = e.Graphics;
+            Font font = new Font("Arial", 8);
+            Brush brush = Brushes.Black;
+            // Draw points and coordinates
+            for (int i = 0; i < PointIndex; i++)
+            {
+                g.FillEllipse(Brushes.Yellow, PX[i] - 6, PY[i] - 6, 10, 10);
+                string coordinates = string.Format("({0}),({1})", PX[i], PY[i]);
+                g.DrawString(coordinates, font, brush, PX[i] + 5, PY[i] - 10);
+            }
+            // Draw De Casteljau points and rectangle
+            if (BtnDeCas1)
+            {
+                for (int i = 2; i <= PointIndex; i++)
+                {
+                    // Take coordinates of points and increment point by point
+                    float[] betaX = new float[i];
+                    float[] betaY = new float[i];
+                    // Copy the coordinates of the points
+                    for (int p = 0; p < i; p++)
+                    {
+                        betaX[p] = PX[p];
+                        betaY[p] = PY[p];
+                    }
+
+                    // Draw De Casteljau points
+                    for (int j = 0; j <= 200; j++)
+                    {
+                        float t = j / 200f; // Decimal division
+                        g.FillEllipse(Brushes.Red, DeCasteljau(t, betaX) - 3, DeCasteljau(t, betaY) - 3, 3, 3);
+                    }
+                }
+                // Determine the coordinates of the rectangle points
+                Point point1 = new Point(MinPoint(PX), MaxPoint(PY));
+                Point point2 = new Point(MaxPoint(PX), MaxPoint(PY));
+                Point point3 = new Point(MaxPoint(PX), MinPoint(PY));
+                Point point4 = new Point(MinPoint(PX), MinPoint(PY));
+                // Draw the rectangle
+                Pen pen = new Pen(Color.Blue, 2);
+                g.DrawLine(pen, point1, point2);
+                g.DrawLine(pen, point2, point3);
+                g.DrawLine(pen, point3, point4);
+                g.DrawLine(pen, point4, point1);
+                pen.Dispose();
+            }
+        }
+        // Activate the clear button
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            PX = new int[25];
+            PY = new int[25];
+            PointIndex = 0;
+            status = false;
+            BtnDeCas1 = false;
+            panel.Invalidate();
+        }
+        // Screen click
+        private void panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (status == true && PointIndex < 25)
+            {
+                PX[PointIndex] = e.X;
+                PY[PointIndex] = e.Y;
+                PointIndex++;
+                panel.Invalidate();
+            }
+            else if (status == false)
+            {
+                MessageBox.Show("Press the button (Draw Points)", "Watch out", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (status == true && PointIndex >= 25)
+            {
+                MessageBox.Show("The number of points exceeded to (25)", "Watch out", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Activate De Casteljau algorithm
+        private void BtnDeCas_Click(object sender, EventArgs e)
+        {
+            if (PointIndex > 1)
+            {
+                status = false;
+                BtnDeCas1 = true;
+                panel.Invalidate();
+            }
+            else if (PointIndex <= 1)
+            {
+                MessageBox.Show("The number of points must be greater than 1 ", "Watch out", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
